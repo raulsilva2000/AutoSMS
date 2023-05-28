0f78dcffa72800e0b18f320eeafc5b6f18de00ef
raulsilva2000 <raul.silva@ua.pt>
2023-05-28 14:54:14 +0100

    [LAYOUT] Header Bar from being in Fragments to Main Activity

--- app/src/main/java/com/example/autosms/ActiveFragment.java
+++ app/src/main/java/com/example/autosms/ActiveFragment.java
@@ -17,22 +17,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ActiveFragment extends Fragment {
     private RecyclerView.Adapter autoSMSAdapter;
     private RecyclerView recyclerViewReplys;
-    DrawerLayout drawerLayout;
-    private ImageView menuIcon;
-    private ImageView closeMenu;
-    private ConstraintLayout homeMenu;
-    private ConstraintLayout newAutoSMS;
-    private ConstraintLayout messagesMenu;
-    private ConstraintLayout editProfileMenu;
-    private ConstraintLayout languageMenu;
-    private ConstraintLayout logoutMenu;
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -49,149 +40,7 @@ public class ActiveFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        drawerLayout = view.findViewById(R.id.drawer_layer);
         initRecyclerview(view);
-
-        menuIcon = view.findViewById(R.id.openDrawerMenu);
-        menuIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                ClickDrawerMenu(view);
-            }
-        });
-
-        closeMenu = view.findViewById(R.id.imageView21);
-        closeMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                closeDrawer(view);
-            }
-        });
-
-        homeMenu = view.findViewById(R.id.homeMenu);
-        homeMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                dashboard(view);
-            }
-        });
-
-        newAutoSMS = view.findViewById(R.id.newautosmsMenu);
-        newAutoSMS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                newAutoSMS(view);
-            }
-        });
-
-        messagesMenu = view.findViewById(R.id.messagesMenu);
-        messagesMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                sentMessages(view);
-            }
-        });
-
-        editProfileMenu = view.findViewById(R.id.editprofileMenu);
-        editProfileMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                editProfile(view);
-            }
-        });
-
-        languageMenu = view.findViewById(R.id.languageMenu);
-        languageMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                language(view);
-            }
-        });
-
-        logoutMenu = view.findViewById(R.id.logoutMenu);
-        logoutMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                logout(view);
-            }
-        });
-    }
-
-    public void ClickDrawerMenu(View view) {
-        openDrawer(drawerLayout);
-    }
-
-    private void openDrawer(DrawerLayout drawerLayout){
-        drawerLayout.openDrawer(GravityCompat.START);
-    }
-
-    public void closeDrawer(View view){
-        drawerLayout.closeDrawer(GravityCompat.START);
-    }
-
-    public void dashboard(View view) {
-        //mudar para fragmento
-        /*
-        Intent intent = new Intent((MainActivity) getActivity(), MainActivity.class);
-        startActivity(intent);*/
-
-    }
-
-    public void newAutoSMS(View view) {
-        /*
-        Intent intent = new Intent((MainActivity) getActivity(), NewAutoSMS.class);
-        startActivity(intent);*/
-    }
-
-    public void sentMessages(View view) {
-        /*
-        Intent intent = new Intent((MainActivity) getActivity(), SentMessages.class);
-        startActivity(intent);*/
-    }
-
-    public void editProfile(View view) {
-        /*
-        Intent intent = new Intent((MainActivity) getActivity(), MainActivity.class);
-        startActivity(intent);*/
-    }
-
-    public void language(View view) {
-        /*
-        Intent intent = new Intent((MainActivity) getActivity(), MainActivity.class);
-        startActivity(intent);*/
-    }
-
-    public void logout(View view) {
-        logoutMenu((MainActivity) getActivity());
-    }
-
-    private void logoutMenu(MainActivity mainActivity) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
-        builder.setTitle("Logout");
-        builder.setMessage("Are you sure you want to logout?");
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                mainActivity.finish();
-            }
-        });
-
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-
-        builder.show();
     }
 
     private void initRecyclerview(View view){
--- app/src/main/java/com/example/autosms/MainActivity.java
+++ app/src/main/java/com/example/autosms/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.autosms;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -19,6 +23,17 @@ public class MainActivity extends AppCompatActivity {
     NewAutoSMSFragment newAutoSMSFragment = new NewAutoSMSFragment();
     SentMessagesFragment sentMessagesFragment = new SentMessagesFragment();
 
+    DrawerLayout drawerLayout;
+    private ImageView menuIcon;
+    private ImageView closeMenu;
+
+    private ConstraintLayout homeMenu;
+    private ConstraintLayout newAutoSMS;
+    private ConstraintLayout messagesMenu;
+    private ConstraintLayout editProfileMenu;
+    private ConstraintLayout languageMenu;
+    private ConstraintLayout logoutMenu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +41,18 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
+        drawerLayout = findViewById(R.id.DrawerLayoutMain);
+
+        menuIcon = findViewById(R.id.openDrawerMenu);
+        closeMenu = findViewById(R.id.imageView21);
+        homeMenu = findViewById(R.id.homeMenu);
+        newAutoSMS = findViewById(R.id.newautosmsMenu);
+        messagesMenu = findViewById(R.id.messagesMenu);
+        editProfileMenu = findViewById(R.id.editprofileMenu);
+        languageMenu = findViewById(R.id.languageMenu);
+        logoutMenu = findViewById(R.id.logoutMenu);
+
+        updateTextViewTitleHeader("Active Replys");
         getSupportFragmentManager().beginTransaction().replace(R.id.containerFrame, activeFragment).commit();
 
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -33,17 +60,158 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
                 if(id == R.id.active){
+                    updateTextViewTitleHeader("Active Replys");
                     getSupportFragmentManager().beginTransaction().replace(R.id.containerFrame, activeFragment).commit();
                     return true;
                 } else if (id == R.id.autoSMS) {
+                    updateTextViewTitleHeader("New AutoSMS Reply");
                     getSupportFragmentManager().beginTransaction().replace(R.id.containerFrame, newAutoSMSFragment).commit();
                     return true;
                 } else if (id == R.id.sent) {
+                    updateTextViewTitleHeader("Sent Messages");
                     getSupportFragmentManager().beginTransaction().replace(R.id.containerFrame, sentMessagesFragment).commit();
                     return true;
                 }
                 return false;
             }
         });
+
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                ClickDrawerMenu(view);
+            }
+        });
+
+        closeMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                closeDrawer(view);
+            }
+        });
+
+        homeMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                dashboard(view);
+            }
+        });
+
+        newAutoSMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                newAutoSMS(view);
+            }
+        });
+
+        messagesMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                sentMessages(view);
+            }
+        });
+
+        editProfileMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                editProfile(view);
+            }
+        });
+
+        languageMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                language(view);
+            }
+        });
+
+        logoutMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                logout(view);
+            }
+        });
+    }
+
+    public void updateTextViewTitleHeader(String text) {
+        TextView textTitleHeader = findViewById(R.id.textViewTitleHeader);
+        textTitleHeader.setText(text);
+    }
+
+    public void ClickDrawerMenu(View view) {
+        openDrawer(drawerLayout);
+    }
+
+    private void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void closeDrawer(View view){
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    public void dashboard(View view) {
+        //mudar para fragmento
+        /*
+        Intent intent = new Intent((MainActivity) getActivity(), MainActivity.class);
+        startActivity(intent);*/
+
+    }
+
+    public void newAutoSMS(View view) {
+        /*
+        Intent intent = new Intent((MainActivity) getActivity(), NewAutoSMS.class);
+        startActivity(intent);*/
+    }
+
+    public void sentMessages(View view) {
+        /*
+        Intent intent = new Intent((MainActivity) getActivity(), SentMessages.class);
+        startActivity(intent);*/
+    }
+
+    public void editProfile(View view) {
+        /*
+        Intent intent = new Intent((MainActivity) getActivity(), MainActivity.class);
+        startActivity(intent);*/
+    }
+
+    public void language(View view) {
+        /*
+        Intent intent = new Intent((MainActivity) getActivity(), MainActivity.class);
+        startActivity(intent);*/
+    }
+
+    public void logout(View view) {
+        logoutMenu((this));
+    }
+
+    private void logoutMenu(MainActivity mainActivity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to logout?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mainActivity.finish();
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
     }
 }
